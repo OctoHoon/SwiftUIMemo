00f0c512ff45a6aa86d5306fbac22bf5e130d789
OctoHoon <sakonghoon@naver.com>
2022-09-05 20:57:42 +0900

    Step9

--- Shared/View/MainListView.swift
+++ Shared/View/MainListView.swift
@@ -14,14 +14,16 @@ struct MainListView: View {
     
     var body: some View {
         NavigationView {
-            List(store.list) { memo in
-                // 우클릭 extracted subview로 view를 분리해 가독성 높임
-                NavigationLink {
-                    DetailView(memo: memo)
-                } label: {
-                    MemoCell(memo: memo)
+            List {
+                ForEach(store.list) { memo in
+                    // 우클릭 extracted subview로 view를 분리해 가독성 높임
+                    NavigationLink {
+                        DetailView(memo: memo)
+                    } label: {
+                        MemoCell(memo: memo)
+                    }
                 }
-                
+                .onDelete(perform: store.delete)
             }
             .listStyle(.plain)
             .navigationTitle("내 메모")
@@ -34,7 +36,7 @@ struct MainListView: View {
             }
             // ispresented true면 함수 실행ㄴ
             .sheet(isPresented: $showComposer){ ComposeView()
-            }
+        }
         }
         
     }
